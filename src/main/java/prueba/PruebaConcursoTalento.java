cef6f160c36b1331a1299aae9c2cd8f61116fef0
Desarrollo2 <devsolutionCo@gmail.com>
2018-11-02 07:43:43 -0500

    ewrwer

--- src/main/java/prueba/PruebaConcursoTalento.java
+++ src/main/java/prueba/PruebaConcursoTalento.java
@@ -1,6 +1,5 @@
 package prueba;
 
-import org.apache.log4j.PropertyConfigurator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,8 +7,7 @@ import concursantes.Concursante;
 
 public class PruebaConcursoTalento {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Concursante malabarista =(Concursante) context.getBean("solei");
